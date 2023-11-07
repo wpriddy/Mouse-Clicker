4ec43ca39c1054bd4f4a5b8315e32e7f5e36255f
Wyatt Priddy <wpriddy@childfund.org>
2023-11-07 11:29:53 -0500

    updating url and version

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='mouse_clicker',
-      version='1.0',
+      version='1.24',
       description='Keep Computer From Hibernating',
       author='Wyatt Priddy',
-      url='https://github.com/wpriddy/M09-In-Class-Activity',
+      url='https://github.com/wpriddy/Mouse-Clicker',
       license='MIT',
       packages=['mouse_clicker'],
       install_requires=['pyautogui==0.9.53']
